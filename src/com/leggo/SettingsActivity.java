c2bcd019d0f06dd90ff04f19e9f440864f8c6171
Russell <brokenspicerack@gmail.com>
2013-10-17 20:35:50 -0400

    Skeleton

---
+++ src/com/leggo/SettingsActivity.java
@@ -0,0 +1,39 @@
+package com.leggo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent i = null;
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			i = new Intent(this, SettingsActivity.class);
+			startActivity(i);
+			break;
+		case R.id.action_manage:
+			i = new Intent(this, ManageActivity.class);
+			startActivity(i);
+			break;
+		}
+		return true;
+	}
+
+}
