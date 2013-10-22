1bc0bae709810927d069f37e6ca3fe00ffd5b9d4
Russell <brokenspicerack@gmail.com>
2013-10-22 16:26:08 -0400

    Implemented account selecting and adding

--- gen/com/leggo/R.java
+++ gen/com/leggo/R.java
@@ -8,6 +8,10 @@
 package com.leggo;
 
 public final class R {
+    public static final class array {
+        public static final int fontSizeOptions=0x7f040000;
+        public static final int fontSizeValues=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,16 +21,16 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int inputboxshape=0x7f020001;
     }
     public static final class id {
-        public static final int action_manage=0x7f080001;
-        public static final int action_settings=0x7f080000;
+        public static final int action_manage=0x7f090001;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,12 +38,17 @@ public final class R {
         public static final int activity_settings=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_manage=0x7f050002;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
+        public static final int account_title=0x7f060005;
+        public static final int action_manage=0x7f060002;
+        public static final int action_settings=0x7f060001;
+        public static final int add_account_title=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int font_size_title=0x7f060007;
+        public static final int general_settings_title=0x7f060004;
+        public static final int google_accounts_title=0x7f060003;
     }
     public static final class style {
         /** 
@@ -63,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
