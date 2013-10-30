78ceb9eb0208dd9f73bcbc63eb1d22a621531183
kgallagher315 <kgall@hunter.cuny.edu>
2013-10-29 22:54:53 -0400

    Merged code
    Merged two branches. Still have some work to do.

--- src/com/leggo/MainActivity.java
+++ src/com/leggo/MainActivity.java
@@ -1,50 +1,74 @@
 package com.leggo;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
+import com.leggo.Article.ArticleSearchResult;
+import com.leggo.parsing.GetArticlesCommand;
+import com.leggo.parsing.GetFeedsCommand;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.graphics.Color;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
-
-import com.leggo.parsing.GetArticlesCommand;
-import com.leggo.parsing.GetFeedsCommand;
-
 public class MainActivity extends Activity {
 
 	public static final String TAG = "MainActivity";
-	
-	private Context context;
+    
+    private Context context;
 
-	private SharedPreferences prefs;
-	
-	private String currentAccountName;
+    private SharedPreferences prefs;
+    
+    private String currentAccountName;
+    List<Article> articles;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		context = this;
+        context = this;
 
-		prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		new Thread() {
-			public void run() {
-				testGetRemoteArticles();
-			}
-		}.start();
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        new Thread() {
+                public void run() {
+                        testGetRemoteArticles();
+                }
+        }.start();
+		loadArticles();
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+		Date now = new Date();
+		TextView refreshBar = (TextView) findViewById(R.id.main_refresh_bar);
+		refreshBar.setText(df.format(now).toString());
 
 	}
 
@@ -52,7 +76,9 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		ActionBar action = getActionBar();
+		action.show();
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -66,6 +92,87 @@ public class MainActivity extends Activity {
 			i = new Intent(this, ManageActivity.class);
 			startActivity(i);
 			break;
+		case R.id.action_search:
+			ActionBar actionBar = getActionBar();
+			//actionBar.hide();
+			actionBar.setCustomView(R.layout.searchbar);
+			EditText search = (EditText) actionBar.getCustomView().findViewById(R.id.action_searchfield);
+			search.setOnEditorActionListener(new OnEditorActionListener() {
+
+			      @Override
+			      public boolean onEditorAction(TextView v, int actionId,
+			          KeyEvent event) {
+			    	  Article searcher = new Article();
+			    	  List<ArticleSearchResult> results = searcher.search(v.getText().toString(),articles);
+			    	  Log.d("LOLOL", v.getText().toString());
+						LinearLayout linearLayout = (LinearLayout)findViewById(R.id.article_list);
+						if(((LinearLayout) linearLayout).getChildCount() > 0) 
+						    ((LinearLayout) linearLayout).removeAllViews();
+  			    	  LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                              LayoutParams.MATCH_PARENT,
+                              LayoutParams.WRAP_CONTENT, 0.10f);
+                      LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(
+                              LayoutParams.MATCH_PARENT,
+                              LayoutParams.WRAP_CONTENT, 0.90f);
+			    	  for(int i = 0; i < 2*results.size(); i+=2)
+			    	  {
+			    		  LinearLayout articleScroll = (LinearLayout)findViewById(R.id.article_list);
+			    		  LinearLayout currArticle = new LinearLayout(getBaseContext());
+	                        currArticle.setOrientation(LinearLayout.HORIZONTAL);
+	                        currArticle.setPadding(5, 5, 5, 5);
+	                        Button articleName = new Button(getBaseContext());
+	                        articleName.setId(i);
+	                        articleName.setText((CharSequence)(results.get(i/2).article.getTitle()));
+	                        articleName.setTextColor(Color.parseColor("#000000"));
+	                        articleName.setGravity(Gravity.LEFT);
+	                        articleName.setBackground(getBaseContext().getResources().getDrawable(R.drawable.textlines));
+	                        articleName.setOnClickListener(new View.OnClickListener() {
+	                        	public void onClick(View v) {
+	                        		int id = v.getId();
+	                        		if(id%2 == 0);
+	                        			//replace with call for view;         
+	                        	}
+	                        });
+	                        articleName.setLayoutParams(param);
+	                        ImageButton peek = new ImageButton(getBaseContext());
+	                        peek.setId(i+1);
+	                        peek.setBackground(getBaseContext().getResources().getDrawable(R.drawable.ic_read));
+	                        peek.setOnClickListener(new View.OnClickListener() {
+						        public void onClick(View v) {
+						                 int id = v.getId();
+						                 if(id%2 == 1);
+						                       //replace with call for peek         
+						        }
+					        });
+	                        peek.setLayoutParams(param2);
+	                        currArticle.addView(articleName);
+	                        currArticle.addView(peek);
+	                        articleScroll.addView(currArticle);
+			    		  
+			    	  }
+			    	  ActionBar actionBar = getActionBar();
+			    	  actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME
+						        | ActionBar.DISPLAY_SHOW_HOME);
+			        return false;
+			      }
+			    });
+			    actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
+			        | ActionBar.DISPLAY_SHOW_HOME);
+			//actionBar.show();
+			
+			break;
+		case R.id.action_refresh:
+			LinearLayout linearLayout = (LinearLayout)findViewById(R.id.article_list);
+			if(((LinearLayout) linearLayout).getChildCount() > 0) 
+			    ((LinearLayout) linearLayout).removeAllViews();
+			GetArticlesCommand refresh = new GetArticlesCommand();
+			GetArticles get = new GetArticles();
+			SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+			get.execute(refresh);
+			Date now = new Date();
+			TextView refreshBar = (TextView) findViewById(R.id.main_refresh_bar);
+			refreshBar.setText(df.format(now).toString());
+			break;
 		}
 		return true;
 	}
@@ -77,37 +184,46 @@ public class MainActivity extends Activity {
 		if (networkAvailability() == false) {
 			noNetworkAlert();
 		}
+		 else {
+             currentAccountName = prefs.getString("account_selection", "default");
+             Toast.makeText(context, "Currently logged in to " + currentAccountName, Toast.LENGTH_SHORT).show();
 
-		else {
-			currentAccountName = prefs.getString("account_selection", "default");
-			Toast.makeText(context, "Currently logged in to " + currentAccountName, Toast.LENGTH_SHORT).show();
+             if (currentAccountName.equals("None") || currentAccountName.equals("default")) {
+                     noAccountAlert();
+             } else {
+                     AccountManager accountManager = AccountManager.get(getApplicationContext());
+                     Account[] accounts = accountManager.getAccountsByType("com.google");
 
-			if (currentAccountName.equals("None") || currentAccountName.equals("default")) {
-				noAccountAlert();
-			} else {
-				AccountManager accountManager = AccountManager.get(getApplicationContext());
-				Account[] accounts = accountManager.getAccountsByType("com.google");
+                     // Find index where account is
+                     int accountIndex = 0;
+                     for (Account account : accounts) {
+                             if (account.name.equals(currentAccountName)) {
+                                     // Things seem to be okay.
+                                     String auth_token = prefs.getString("token", "default");
+                                     String cookie = prefs.getString("cookie", "default");
+                                     Toast.makeText(context, "AuthToken:" + auth_token, Toast.LENGTH_SHORT).show();
+                                     Toast.makeText(context, "Cookie:" + cookie, Toast.LENGTH_SHORT).show();
+                             }
+                             accountIndex++;
+                     }
 
-				// Find index where account is
-				int accountIndex = 0;
-				for (Account account : accounts) {
-					if (account.name.equals(currentAccountName)) {
-						// Things seem to be okay.
-						String auth_token = prefs.getString("token", "default");
-						String cookie = prefs.getString("cookie", "default");
-						Toast.makeText(context, "AuthToken:" + auth_token, Toast.LENGTH_SHORT).show();
-						Toast.makeText(context, "Cookie:" + cookie, Toast.LENGTH_SHORT).show();
-					}
-					accountIndex++;
-				}
-
-				if (accountIndex < accounts.length) {
-					// Account doesn't exist anymore for some reason.
-					noAccountAlert();
-				}
-			}
-		}
+                     if (accountIndex < accounts.length) {
+                             // Account doesn't exist anymore for some reason.
+                             noAccountAlert();
+                     }
+             }
+     }
 	}
+	
+	private void loadArticles()
+    {
+    	if(networkAvailability()) //until login is taken care of
+        {
+    		GetArticles get = new GetArticles();
+    		GetArticlesCommand command = new GetArticlesCommand();
+    		get.execute(command);
+        }
+     }  
 
 	private boolean networkAvailability() {
 		ConnectivityManager CM = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -115,63 +231,136 @@ public class MainActivity extends Activity {
 		return active != null && active.isConnected();
 	}
 
-	private void noNetworkAlert() {
-		new AlertDialog.Builder(this).setTitle("No Network Connection").setMessage("leggo cannot detect a network connection on this device.  Please check Network Settings to connect to an available network to use leggo.").setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				// nothing
+    private void noNetworkAlert() {
+        new AlertDialog.Builder(this).setTitle("No Network Connection").setMessage("leggo cannot detect a network connection on this device. Please check Network Settings to connect to an available network to use leggo.").setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                        // nothing
+                }
+        }).show();
+    }
+
+    private void noAccountAlert() {
+        new AlertDialog.Builder(this).setTitle("No Google Account Selected").setMessage("leggo requires a Google Account to store your subscriptions. Please select an existing account or create an account in Settings.").setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                        Intent i = null;
+                        i = new Intent(context, SettingsActivity.class);
+                        startActivity(i);
+                }
+        }).show();
+    }
+	
+	public void listArticles() {
+		if(articles != null)
+        {
+        	Log.d("ARTICLES", "Here " + articles.size());
+            LinearLayout articleScroll = (LinearLayout)findViewById(R.id.article_list);
+	    	LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                      LayoutParams.MATCH_PARENT,
+                      LayoutParams.WRAP_CONTENT, 0.10f);
+            LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(
+                      LayoutParams.MATCH_PARENT,
+                      LayoutParams.WRAP_CONTENT, 0.90f);
+                for(int i = 0; i < 2*articles.size(); i += 2)
+                {
+                        LinearLayout currArticle = new LinearLayout(this);
+                        currArticle.setOrientation(LinearLayout.HORIZONTAL);
+                        currArticle.setPadding(100000, 100000, 100000, 100000);
+                        currArticle.setBackground(this.getResources().getDrawable(R.drawable.box));
+                        Button articleName = new Button(this);
+                        articleName.setId(i);
+                        articleName.setText((CharSequence)(articles.get(i/2).getTitle()));
+                        articleName.setBackground(getResources().getDrawable(R.drawable.roundbutton));
+                        articleName.setTextColor(Color.parseColor("#000000"));
+                        articleName.setGravity(Gravity.LEFT);
+                        articleName.setOnClickListener(new View.OnClickListener() {
+                        	public void onClick(View v) {
+                        		int id = v.getId();
+                        		if(id%2 == 0);
+                        			//replace with call for view;         
+                        	}
+                        });
+                        articleName.setLayoutParams(param);
+                        ImageButton peek = new ImageButton(this);
+                        peek.setId(i+1);
+                        peek.setBackground(getBaseContext().getResources().getDrawable(R.drawable.ic_read));
+                        peek.setOnClickListener(new View.OnClickListener() {
+					        public void onClick(View v) {
+					                 int id = v.getId();
+					                 if(id%2 == 1);
+					                       //replace with call for peek         
+					        }
+				        });
+                        peek.setLayoutParams(param2);
+                        currArticle.addView(articleName);
+                        currArticle.addView(peek);
+                        articleScroll.addView(currArticle);
+                }
+        }
+	}
+	
+   public Object testGetArticles() {
+        Object result = null;
+        try {
+                File dir = this.getFilesDir();
+                String fileName = "simplecta_all.htm";
+                String testFile = new File(dir + File.separator + fileName).getAbsolutePath();
+                GetArticlesCommand cmd = new GetArticlesCommand();
+                result = cmd.testFromFile(testFile);
+        } catch (Throwable ex) {
+                Log.e(TAG, ex.getMessage(), ex);
+        }
+        return result;
+    }
+    
+    public Object testGetFeeds() {
+        Object result = null;
+        try {
+                File dir = this.getFilesDir();
+                String fileName = "simplecta_feeds.htm";
+                String testFile = new File(dir + File.separator + fileName).getAbsolutePath();
+                GetFeedsCommand cmd = new GetFeedsCommand();
+                result = cmd.testFromFile(testFile);
+        } catch (Throwable ex) {
+                Log.e(TAG, ex.getMessage(), ex);
+        }
+        return result;
+    }
+    
+    public Object testGetRemoteArticles() {
+        Object result = null;
+        try {
+                GetArticlesCommand cmd = new GetArticlesCommand();
+                String c = prefs.getString("cookie", "default");
+                result = cmd.parseData(c);
+                Log.d(TAG, "RESULT:" + result);
+        } catch (Throwable ex) {
+                Log.e(TAG, ex.getMessage(), ex);
+        }
+        return result;
+    }
+	
+	protected class GetArticles extends AsyncTask<GetArticlesCommand, Integer, List<Article>> {
+		@Override
+		protected List<Article> doInBackground(GetArticlesCommand... params)
+		{
+			GetArticlesCommand get = params[0];
+			List<Article> articles = null;
+			try{
+				articles = (List<Article>)get.parseData();
+				Log.d("ARTICLES","In try " + articles.size());
+			} catch(IOException e) {
+				Log.d("ARTICLES", "IOException caught");
+				return null;
 			}
-		}).show();
-	}
-
-	private void noAccountAlert() {
-		new AlertDialog.Builder(this).setTitle("No Google Account Selected").setMessage("leggo requires a Google Account to store your subscriptions.  Please select an existing account or create an account in Settings.").setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				Intent i = null;
-				i = new Intent(context, SettingsActivity.class);
-				startActivity(i);
-			}
-		}).show();
-	}
-
-	public Object testGetArticles() {
-		Object result = null;
-		try {
-			File dir = this.getFilesDir();
-			String fileName = "simplecta_all.htm";
-			String testFile = new File(dir + File.separator + fileName).getAbsolutePath();
-			GetArticlesCommand cmd = new GetArticlesCommand();
-			result = cmd.testFromFile(testFile);
-		} catch (Throwable ex) {
-			Log.e(TAG, ex.getMessage(), ex);
+			return articles;
 		}
-		return result;
-	}
-
-	public Object testGetFeeds() {
-		Object result = null;
-		try {
-			File dir = this.getFilesDir();
-			String fileName = "simplecta_feeds.htm";
-			String testFile = new File(dir + File.separator + fileName).getAbsolutePath();
-			GetFeedsCommand cmd = new GetFeedsCommand();
-			result = cmd.testFromFile(testFile);
-		} catch (Throwable ex) {
-			Log.e(TAG, ex.getMessage(), ex);
-		}
-		return result;
-	}
-
-	public Object testGetRemoteArticles() {
-		Object result = null;
-		try {
-			GetArticlesCommand cmd = new GetArticlesCommand();
-			String c = prefs.getString("cookie", "default");
-			result = cmd.parseData(c);
-			Log.d(TAG, "RESULT:" + result);
-		} catch (Throwable ex) {
-			Log.e(TAG, ex.getMessage(), ex);
-		}
-		return result;
+		
+		@Override
+	     protected void onPostExecute(List<Article> result) {
+			Log.d("ARTICLES", "On Post Execute "+result.size());
+			MainActivity.articles = result;
+			listArticles();  
+	     }
 	}
 
 }
