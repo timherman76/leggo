ffde28aabbe742f0252ef91c1184f3e573b0b6d9
Russell <brokenspicerack@gmail.com>
2013-11-21 18:56:43 -0500

    account check on manage feeds

--- src/com/leggo/ManageActivity.java
+++ src/com/leggo/ManageActivity.java
@@ -47,18 +47,20 @@ import com.leggo.parsing.GetFeedsCommand;
 import com.leggo.parsing.UnsubscribeCommand;
 
 public class ManageActivity extends Activity {
-	
+
 	private static List<Feed> allFeeds = null;
-	
+
 	private File sdCard = Environment.getExternalStorageDirectory();
 	public static File filesDir;
 	private SharedPreferences prefs;
 	public static boolean isAdded;
 	private Context context;
 	private Vibrator myVib;
-	
+
 	private int fontSize;
-	
+
+	String currentAccountName;
+
 	public static boolean shouldRestart;
 
 	@Override
@@ -73,10 +75,20 @@ public class ManageActivity extends Activity {
 
 		filesDir = new File(sdCard + "/Android/data/com.leggo/files");
 		filesDir.mkdirs();
-		loadFeeds();
-		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+		currentAccountName = prefs.getString("account_selection", "default");
+		if (currentAccountName.equals("None")
+				|| currentAccountName.equals("default")) {
+			Utils.noAccountAlert(this);
+		}
+
+		else {
+			loadFeeds();
+			
+		}
 		Date now = new Date();
-		
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+
+
 		shouldRestart = false;
 
 		setContentView(R.layout.activity_manage);
@@ -92,18 +104,18 @@ public class ManageActivity extends Activity {
 		getMenuInflater().inflate(R.menu.manage, menu);
 		return true;
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		
+
 		fontSize = Integer.parseInt(prefs.getString("fontSize", "16"));
 
 		if (shouldRestart == true) {
 			Utils.restartActivity(this);
 			shouldRestart = false;
 		}
-		
+
 	}
 
 	@SuppressWarnings("unchecked")
@@ -128,36 +140,33 @@ public class ManageActivity extends Activity {
 		}
 	}
 
-	
 	public void onEnterSearchText(View v) {
 		EditText uri = (EditText) findViewById(R.id.add_feed_uri);
 		String text = uri.getText().toString();
-		
+
 		if (text == null || text.isEmpty()) {
-			Toast warning = Toast.makeText(this, "Please enter a feed url or search expression.", Toast.LENGTH_SHORT);
+			Toast warning = Toast.makeText(this,
+					"Please enter a feed url or search expression.",
+					Toast.LENGTH_SHORT);
 			warning.show();
 			return;
 		}
-		
-		
-		if(text.startsWith("http://") || text.startsWith("https://"))
-		{
-			//if user entered a link then try to add feed
+
+		if (text.startsWith("http://") || text.startsWith("https://")) {
+			// if user entered a link then try to add feed
 			addFeed(text);
-		}
-		else
-		{
-			//assume user was trying to search for feeds...
+		} else {
+			// assume user was trying to search for feeds...
 			searchFeeds(text);
 		}
-		
+
 	}
-	
+
 	public void addFeed(String feedUrl) {
-		
+
 		String addlink;
 		try {
-			feedUrl =  URLDecoder.decode(feedUrl, "UTF-8");
+			feedUrl = URLDecoder.decode(feedUrl, "UTF-8");
 			feedUrl = URLEncoder.encode(feedUrl, "UTF-8");
 			addlink = ("addRSS/?url=" + feedUrl);
 		} catch (UnsupportedEncodingException e) {
@@ -172,40 +181,44 @@ public class ManageActivity extends Activity {
 			loadFeeds();
 			MainActivity.shouldRefresh = true;
 		} else {
-			Toast warning = Toast.makeText(this, "Please connect to the internet to add a feed.", Toast.LENGTH_SHORT);
+			Toast warning = Toast.makeText(this,
+					"Please connect to the internet to add a feed.",
+					Toast.LENGTH_SHORT);
 			warning.show();
 			return;
 		}
 
 	}
 
-	
-
 	private void listFeeds() {
 		if (allFeeds != null) {
 			Log.d("FEEDS", "Here " + allFeeds.size());
 			LinearLayout feedScroll = (LinearLayout) findViewById(R.id.feed_list);
-			
+
 			// CurrFeed Layout Params
 			TableLayout.LayoutParams currFeedParam = new TableLayout.LayoutParams(
-					TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
-			
+					TableLayout.LayoutParams.MATCH_PARENT,
+					TableLayout.LayoutParams.WRAP_CONTENT);
+
 			// Feed Name Params
-			TableRow.LayoutParams feedNameParam = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+			TableRow.LayoutParams feedNameParam = new TableRow.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
 			feedNameParam.setMargins(10, 0, 10, 0);
-			
+
 			// Button Params
-			TableRow.LayoutParams buttonParam = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			TableRow.LayoutParams buttonParam = new TableRow.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 			buttonParam.setMargins(10, 10, 10, 10);
-			
+
 			// Divider Params
 			RelativeLayout.LayoutParams dividerParam = new RelativeLayout.LayoutParams(
 					RelativeLayout.LayoutParams.MATCH_PARENT, 1);
-			
-			if (((LinearLayout) feedScroll).getChildCount() > 0) ((LinearLayout) feedScroll).removeAllViews();
+
+			if (((LinearLayout) feedScroll).getChildCount() > 0)
+				((LinearLayout) feedScroll).removeAllViews();
 			for (int i = 0; i < 2 * allFeeds.size(); i += 2) {
 				Feed feed = allFeeds.get(i / 2);
-				if(feed.isAdded()){
+				if (feed.isAdded()) {
 					myVib.vibrate(50);
 					TableLayout currFeed = new TableLayout(this);
 					TableRow tableRow1 = new TableRow(this);
@@ -226,33 +239,35 @@ public class ManageActivity extends Activity {
 					});
 					ImageButton unsubscribe = new ImageButton(this);
 					unsubscribe.setId(i + 1);
-					Drawable icon = getResources().getDrawable(R.drawable.ic_menu_delete);
+					Drawable icon = getResources().getDrawable(
+							R.drawable.ic_menu_delete);
 					unsubscribe.setBackground(icon);
 					unsubscribe.setOnClickListener(new View.OnClickListener() {
 						public void onClick(View v) {
 							int id = v.getId();
 							myVib.vibrate(50);
-							String unsubURL = "unsubscribe/?" + allFeeds.get((id-1)/2).getKey();
-							UnsubscribeCommand unsub = new UnsubscribeCommand(unsubURL);
+							String unsubURL = "unsubscribe/?"
+									+ allFeeds.get((id - 1) / 2).getKey();
+							UnsubscribeCommand unsub = new UnsubscribeCommand(
+									unsubURL);
 							RemoveFeed remove = new RemoveFeed();
 							remove.execute(unsub);
-							allFeeds.remove((id-1)/2);
-							MainActivity.shouldRefresh=true;
+							allFeeds.remove((id - 1) / 2);
+							MainActivity.shouldRefresh = true;
 							listFeeds();
-							
+
 						}
 					});
 					View divider = new View(this);
 					divider.setBackgroundColor(Color.parseColor("#9E9E9E"));
-					
+
 					feedScroll.addView(currFeed, currFeedParam);
 					currFeed.addView(tableRow1, currFeedParam);
 					tableRow1.addView(feedName, feedNameParam);
 					tableRow1.addView(unsubscribe, buttonParam);
-					currFeed.addView(divider,dividerParam);
-					
-				}
-				else {
+					currFeed.addView(divider, dividerParam);
+
+				} else {
 					TableLayout currFeed = new TableLayout(this);
 					TableRow tableRow1 = new TableRow(this);
 					TextView feedName = new TextView(this);
@@ -273,29 +288,31 @@ public class ManageActivity extends Activity {
 					});
 					ImageButton addFeed = new ImageButton(this);
 					addFeed.setId(i + 1);
-					Drawable icon = getResources().getDrawable(R.drawable.btn_check_on);
+					Drawable icon = getResources().getDrawable(
+							R.drawable.btn_check_on);
 					addFeed.setBackground(icon);
 					addFeed.setOnClickListener(new View.OnClickListener() {
 						public void onClick(View v) {
 							myVib.vibrate(50);
 							int id = v.getId();
-							String addFeedURL = allFeeds.get((id-1)/2).getURL();
+							String addFeedURL = allFeeds.get((id - 1) / 2)
+									.getURL();
 							addFeed(addFeedURL);
-							MainActivity.shouldRefresh=true;
+							MainActivity.shouldRefresh = true;
 							listFeeds();
-							
+
 						}
 					});
 					View divider = new View(this);
 					divider.setBackgroundColor(Color.parseColor("#9E9E9E"));
-					
+
 					feedScroll.addView(currFeed, currFeedParam);
 					currFeed.addView(tableRow1, currFeedParam);
 					tableRow1.addView(feedName, feedNameParam);
 					tableRow1.addView(addFeed, buttonParam);
 					currFeed.addView(divider, dividerParam);
 				}
-				
+
 			}
 		}
 	}
@@ -311,26 +328,31 @@ public class ManageActivity extends Activity {
 			ActionBar actionBar = getActionBar();
 			// actionBar.hide();
 			actionBar.setCustomView(R.layout.searchbar);
-			EditText search = (EditText) actionBar.getCustomView().findViewById(R.id.action_searchfield);
+			EditText search = (EditText) actionBar.getCustomView()
+					.findViewById(R.id.action_searchfield);
 			search.setOnEditorActionListener(new OnEditorActionListener() {
 
 				@Override
-				public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				public boolean onEditorAction(TextView v, int actionId,
+						KeyEvent event) {
 					allFeeds = Feed.search(v.getText().toString(), allFeeds);
 					Log.d("FEED SEARCH", v.getText().toString());
-					
+
 					ActionBar actionBar = getActionBar();
-					actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_HOME);
+					actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME
+							| ActionBar.DISPLAY_SHOW_HOME);
 					return false;
 				}
 			});
-			actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
+			actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
+					| ActionBar.DISPLAY_SHOW_HOME);
 			// actionBar.show();
 
 			break;
 		case R.id.action_refresh:
 			LinearLayout linearLayout = (LinearLayout) findViewById(R.id.feed_list);
-			if (((LinearLayout) linearLayout).getChildCount() > 0) ((LinearLayout) linearLayout).removeAllViews();
+			if (((LinearLayout) linearLayout).getChildCount() > 0)
+				((LinearLayout) linearLayout).removeAllViews();
 			GetFeedsCommand refresh = new GetFeedsCommand();
 			GetFeeds get = new GetFeeds(this);
 			get.execute(refresh);
@@ -343,30 +365,29 @@ public class ManageActivity extends Activity {
 		return true;
 	}
 
-	
-
 	public void viewFeed(Feed feed) {
 		Log.d("FEEDS", "Yes, the feed will be launched");
 		// TODO: implement this. Might need to add a new activity.
 	}
 
-	protected class SearchFeeds extends AsyncTask<FeedSearchCommand, Integer, List<Feed>> {
+	protected class SearchFeeds extends
+			AsyncTask<FeedSearchCommand, Integer, List<Feed>> {
 		Context c;
 		ProgressDialog dialog;
 		String searchText;
-		
-		public SearchFeeds(Context context, String searchText){
+
+		public SearchFeeds(Context context, String searchText) {
 			c = context;
 			dialog = new ProgressDialog(c);
 			this.searchText = searchText;
 		}
-		
-		
+
 		@Override
-		public void onPreExecute(){
+		public void onPreExecute() {
 			dialog.setMessage("Loading Feeds");
 			dialog.show();
 		}
+
 		@SuppressWarnings("unchecked")
 		@Override
 		protected List<Feed> doInBackground(FeedSearchCommand... params) {
@@ -384,8 +405,8 @@ public class ManageActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(List<Feed> result) {
-			
-			if(dialog != null && dialog.isShowing()) {
+
+			if (dialog != null && dialog.isShowing()) {
 				dialog.dismiss();
 			}
 			Log.d("FEEDS", "On Post Execute " + result.size());
@@ -395,24 +416,22 @@ public class ManageActivity extends Activity {
 		}
 	}
 
-
-	
-	
-	protected class GetFeeds extends AsyncTask<GetFeedsCommand, Integer, List<Feed>> {
+	protected class GetFeeds extends
+			AsyncTask<GetFeedsCommand, Integer, List<Feed>> {
 		Context c;
 		ProgressDialog dialog;
-		
-		public GetFeeds(Context context){
+
+		public GetFeeds(Context context) {
 			c = context;
 			dialog = new ProgressDialog(c);
 		}
-		
-		
+
 		@Override
-		public void onPreExecute(){
+		public void onPreExecute() {
 			dialog.setMessage("Loading Feeds");
 			dialog.show();
 		}
+
 		@SuppressWarnings("unchecked")
 		@Override
 		protected List<Feed> doInBackground(GetFeedsCommand... params) {
@@ -431,7 +450,7 @@ public class ManageActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(List<Feed> result) {
-			if(dialog.isShowing()) {
+			if (dialog.isShowing()) {
 				dialog.dismiss();
 			}
 			Log.d("FEEDS", "On Post Execute " + result.size());
@@ -461,32 +480,35 @@ public class ManageActivity extends Activity {
 		protected void onPostExecute(Boolean success) {
 			ManageActivity.isAdded = (boolean) success;
 			if (!success) {
-				Toast failure = Toast.makeText(getBaseContext(), "This URL is invalid. Please try again.", Toast.LENGTH_SHORT);
+				Toast failure = Toast.makeText(getBaseContext(),
+						"This URL is invalid. Please try again.",
+						Toast.LENGTH_SHORT);
 				failure.show();
 			}
 			listFeeds();
 		}
 	}
-	
-	public class RemoveFeed extends AsyncTask<UnsubscribeCommand, Integer, Boolean>{
+
+	public class RemoveFeed extends
+			AsyncTask<UnsubscribeCommand, Integer, Boolean> {
 		private boolean success = false;
-		
+
 		@Override
-		public Boolean doInBackground(UnsubscribeCommand... params){
+		public Boolean doInBackground(UnsubscribeCommand... params) {
 			UnsubscribeCommand unsub = params[0];
-			try{
+			try {
 				String c = prefs.getString("cookie", "default");
 				success = (Boolean) unsub.parseData(c);
-			} catch(IOException e) {
+			} catch (IOException e) {
 				Log.e("FEEDS", "IOException caught", e);
 				return false;
 			}
 			return (Boolean) success;
 		}
 	}
-	
+
 	@Override
-	public void onConfigurationChanged(Configuration newConfig){
+	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		setContentView(R.layout.activity_manage);
 		listFeeds();
