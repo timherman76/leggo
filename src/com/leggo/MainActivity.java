1ff4fc9281cbdbf5cf6e81504c37f5e5058a7712
Russell <brokenspicerack@gmail.com>
2013-12-10 23:39:20 -0500

    revert last changes

--- src/com/leggo/MainActivity.java
+++ src/com/leggo/MainActivity.java
@@ -234,7 +234,7 @@ public class MainActivity extends Activity {
 			// Article Name Params
 			TableRow.LayoutParams articleNameParam = new TableRow.LayoutParams(
 					TableRow.LayoutParams.WRAP_CONTENT,
-					80, 1f);
+					TableRow.LayoutParams.WRAP_CONTENT, 1f);
 			articleNameParam.setMargins(10, 0, 10, 0);
 
 			// Feed Name Params
